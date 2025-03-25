d22bc2ae0bf991200fffaf378c7dae6f9f06a0b0
ValentinSaraiva-MMI <valentin.saraiva@edu.univ-fcomte.fr>
2025-03-25 12:07:25 +0100

    Fix class attribute usage in SimpleForm component

--- src/components/Forms/index.jsx
+++ src/components/Forms/index.jsx
@@ -17,30 +17,30 @@ const SimpleForm = () => {
       <form data-testid="form" className="form">
         <div className="formGroup">
           <label htmlFor="Lastname" className="label">Enter your Last name: </label>
-          <input type="text" name="Lastname" id="Lastname" required class="input" />
+          <input type="text" name="Lastname" id="Lastname" required className="input" />
         </div>
         <div className="formGroup">
           <label htmlFor="name" className="label">Enter your First name: </label>
-          <input type="text" name="Firstname" id="name" required class="input" />
+          <input type="text" name="Firstname" id="name" required className="input" />
         </div>
         <div className="formGroup">
           <label htmlFor="email" className="label">Enter your email: </label>
-          <input type="email" name="email" id="email" required class="input" />
+          <input type="email" name="email" id="email" required className="input" />
         </div>
         <div className="formGroup">
           <label htmlFor="date" className="label">Enter your date of birth: </label>
-          <input type="date" name="date" id="date" required class="input" />
+          <input type="date" name="date" id="date" required className="input" />
         </div>
         <div className="formGroup">
           <label htmlFor="city" className="label">Enter your City: </label>
-          <input type="city" name="city" id="city" required class="input" />
+          <input type="city" name="city" id="city" required className="input" />
         </div>
         <div className="formGroup">
           <label htmlFor="postalCode" className="label">Enter your postal code: </label>
-          <input type="city" name="postalCode" id="postalCode" required class="input" />
+          <input type="city" name="postalCode" id="postalCode" required className="input" />
         </div>
         <div className="formGroup">
-          <input type="submit" value="Save" class="button" />
+          <input type="submit" value="Save" className="button" />
         </div>
       </form>
     </div>
